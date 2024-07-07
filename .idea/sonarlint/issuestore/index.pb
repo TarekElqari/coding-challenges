f15f88ccc49a564964acf4fbebc574897b696a55
TarekElqari <telqari@gmail.com>
2024-07-07 12:45:54 +0100

    add conf props to hide paths

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,8 @@ S
 -src/main/java/org/example/wc/WordCounter.java,5\e\5e42c2c2e83be88c1dcf87ea1b64196e9bf6d1fb
 V
 &src/main/java/org/example/wc/Main.java,a\3\a3c7fda9ca0d31d45499f75b0bdc8d1b015776d8
+A
+config.properties,5\b\5b4197fb87e6e5ff44afbf358a3bd23a35f56456
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
--- src/main/java/org/example/wc/Main.java
+++ src/main/java/org/example/wc/Main.java
@@ -1,12 +1,24 @@
 package org.example.wc;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        int charCount = WordCounter.howManyCharsInTheFile("C:\\Users\\Tarek-PC\\Desktop\\test.txt");
-        LOGGER.log(Level.INFO, () -> "Number of characters in the file: " + charCount);
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("config.properties"));
+            String filePath = properties.getProperty("file.path");
+
+            int charCount = WordCounter.howManyCharsInTheFile(filePath);
+            LOGGER.log(Level.INFO, () -> "Number of characters in the file: " + charCount);
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Could not read configuration file", e);
+        }
     }
 }
