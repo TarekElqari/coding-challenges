482c36f2ab8d19ae05b54bba262f9b313c2829bc
TarekElqari <telqari@gmail.com>
2024-07-07 13:50:55 +0100

    new features

--- src/main/java/org/example/wc/Main.java
+++ src/main/java/org/example/wc/Main.java
@@ -11,12 +11,18 @@ public class Main {
 
     public static void main(String[] args) {
         Properties properties = new Properties();
-        try {
-            properties.load(new FileInputStream("config.properties"));
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            properties.load(fis);
             String filePath = properties.getProperty("file.path");
+            long charCount = WordCounter.howManyCharsInTheFile(filePath);
+            long lineCount = WordCounter.howManyLinesInTheFile(filePath);
+            long sizeInBytes = WordCounter.getFileSizeInBytes(filePath);
+            long wordsCount = WordCounter.howManyWords(filePath);
 
-            int charCount = WordCounter.howManyCharsInTheFile(filePath);
             LOGGER.log(Level.INFO, () -> "Number of characters in the file: " + charCount);
+            LOGGER.log(Level.INFO, () -> "Number of lines in the file: " + lineCount);
+            LOGGER.log(Level.INFO, () -> "Number of words in the file: " + wordsCount);
+            LOGGER.log(Level.INFO, () -> "Size of the file in bytes : " + sizeInBytes +" bytes");
         } catch (IOException e) {
             LOGGER.log(Level.SEVERE, "Could not read configuration file", e);
         }
--- src/main/java/org/example/wc/WordCounter.java
+++ src/main/java/org/example/wc/WordCounter.java
@@ -1,5 +1,7 @@
 package org.example.wc;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -7,14 +9,11 @@ import java.util.logging.Logger;
 
 public class WordCounter {
     private static final Logger LOGGER = Logger.getLogger(WordCounter.class.getName());
-
-    public static int howManyCharsInTheFile(String pathToFile) {
-        int charsCounter = 0;
-        try (FileReader fr = new FileReader(pathToFile)) {
-            int charRead;
-            // Read each character until the end of the file is reached
-            while ((charRead = fr.read()) != -1) {
-                // Check if the character is not a whitespace
+    public static long howManyCharsInTheFile(String filePath) {
+        long charsCounter = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            long charRead;
+            while ((charRead = br.read()) != -1) {
                 if (!Character.isWhitespace((char) charRead)) {
                     charsCounter++;
                 }
@@ -24,4 +23,40 @@ public class WordCounter {
         }
         return charsCounter;
     }
+    public static long howManyLinesInTheFile(String filePath) {
+        long lineCounter = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (!line.isEmpty()) {
+                    lineCounter++;
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, () -> "An error occurred while reading the file: " + e.getMessage());
+        }
+        return lineCounter;
+    }
+    public static long howManyWords(String filePath) {
+        long numberOfWords = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                numberOfWords += words.length;
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, () -> "An error occurred while reading the file: " + e.getMessage());
+        }
+        return numberOfWords;
+    }
+    public static long getFileSizeInBytes(String filePath) {
+        File file = new File(filePath);
+        if (file.exists() && file.isFile()) {
+            return file.length();
+        } else {
+            throw new IllegalArgumentException("File does not exist or is not a regular file: " + filePath);
+        }
+    }
 }
+
